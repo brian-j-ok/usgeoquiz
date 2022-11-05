5de83279766cddfd0739d86a51ed7713acbcb79b
Brian O'Kelly <brianjrok@gmail.com>
2022-11-05 13:56:13 -0600

    refactored game logic into two functions to handle logic for checking awnsers and drawing + handling duplicates and string transformation

--- main.py
+++ main.py
@@ -11,18 +11,31 @@ data = pandas.read_csv("50_states.csv")
 
 GUESSED_STATES = []
 
-while len(GUESSED_STATES) < 50:
-    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+def check_input(answer_state):
+    if answer_state not in GUESSED_STATES:
+        state_row = data.loc[data['state'] == answer_state]
+        state_name = state_row.state.item()
+        state_xpos = state_row.x.item()
+        state_ypos = state_row.y.item()
 
-    state_row = data.loc[data['state'] == answer_state]
-    state_name = state_row.state.item()
-    state_xpos = state_row.x.item()
-    state_ypos = state_row.y.item()
+        GUESSED_STATES.append(state_name)
 
+        draw_answer(state_name, state_xpos, state_ypos)
+
+def draw_answer(name, x, y):
     state_turtle = turtle.Turtle()
     state_turtle.penup()
     state_turtle.hideturtle()
-    state_turtle.setposition(int(state_xpos), int(state_ypos))
-    state_turtle.write(f"{state_name}", align="center", font=('Arial', 8, 'normal'))
+    state_turtle.setposition(int(x), int(y))
+    state_turtle.write(f"{name}", align="center", font=('Arial', 8, 'normal'))
+
+
+while len(GUESSED_STATES) < 50:
+    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").capitalize()
+
+    check_input(answer_state)
+
+    print(len(GUESSED_STATES))
+    print(GUESSED_STATES)
 
 turtle.mainloop()
